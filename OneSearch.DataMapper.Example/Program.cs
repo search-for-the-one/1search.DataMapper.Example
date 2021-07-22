ffd04466f8c2f27631ec7f2ca687111066e8c73d
Yuxuan Lin <eric.lin@carsales.com.au>
2021-07-22 16:51:35 +1000

    init

---
+++ OneSearch.DataMapper.Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace OneSearch.DataMapper.Example
+{
+    class Program
+    {
+        private static async Task<int> Main(string[] args)
+        {
+            return await new Startup().RunAsync<DataMapperApp>();
+        }
+    }
+}
